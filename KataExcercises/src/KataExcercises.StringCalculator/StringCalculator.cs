0e5a96f15fcbd836dc08fa93d27cd22cc04e7bc5
Marcin Wasilewski <marcin.wasilewski@peoplevox.co.uk>
2017-02-17 00:02:00 +0100

    Added String Calculator project

---
+++ KataExcercises/src/KataExcercises.StringCalculator/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace KataExcercises.StringCalculator
+{
+    public class StringCalculator
+    {
+        public static string Add(int x, int y)
+        {
+            return (x + y).ToString();
+        }
+    }
+}
---
+++ KataExcercises/src/KataExcercises.Tests/StringCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KataExcercises.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void AddTwoNumbersShouldReturnSumAsAString()
+        {
+            //arrange
+
+            //act
+            var result = StringCalculator.StringCalculator.Add(2, 5);
+
+            //assert
+            Assert.Equal("7", result);
+        }
+        
+    }
+}
