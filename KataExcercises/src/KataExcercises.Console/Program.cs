ca223e170c9796fdd7edad0c3714967c344d5756
Marcin Wasilewski <marcin.wasilewski@peoplevox.co.uk>
2017-02-16 23:50:12 +0100

    Initial project

---
+++ KataExcercises/src/KataExcercises.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KataExcercises.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("== Kata Excercises with .NET Core ==");
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ KataExcercises/src/KataExcercises.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KataExcercises.Console")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ccb4d886-047b-4278-b4a0-db37c9a03b9f")]
